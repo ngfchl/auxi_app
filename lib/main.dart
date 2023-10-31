ae78a8b20bfba37b9b4e663d37e2162e59e8124d
ngfchl <328176843@qq.com>
2023-10-31 13:51:25 +0800

    完善GetMaterialApp信息以及状态栏颜色处理

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:auxi_app/utils/storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -9,12 +10,20 @@ void main() async {
   await GetStorage.init();
   // 初始化 持久化数据信息
   await SPUtil.getInstance();
+  // 初始化插件前需要在runApp之前调用初始化代码
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 固定写法，处理状态栏背景颜色
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(
     GetMaterialApp(
         title: "PTOOLS",
         defaultTransition: Transition.cupertino,
         debugShowCheckedModeBanner: false,
         initialRoute: AppPages.INITIAL,
+        navigatorKey: Get.key,
         getPages: AppPages.routes,
         routingCallback: (routing) {
           print('当前路由：${routing!.current.toString()}');
