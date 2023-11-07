dc558ffa7bd214483c4b5508888e11bc837d7f3c
ngfchl <328176843@qq.com>
2023-11-07 14:52:14 +0800

    展示系统配置文件内容

--- lib/app/home/pages/setting.dart
+++ lib/app/home/pages/setting.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:getwidget/components/button/gf_button.dart';
 import 'package:getwidget/shape/gf_button_shape.dart';
 
 import '../../../../../utils/storage.dart';
+import 'controller/setting_controller.dart';
 
 class SettingPage extends StatelessWidget {
-  const SettingPage({super.key, param});
+  SettingPage({super.key, param});
+
+  final controller = Get.put(SettingController());
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +20,30 @@ class SettingPage extends StatelessWidget {
         children: [
           GFButton(
             onPressed: () {
+              TextEditingController configController = TextEditingController();
+              configController.text = controller.configData;
+              Get.defaultDialog(
+                  title: '配置项',
+                  content: TextField(
+                    controller: configController,
+                    maxLines: 24,
+                  ),
+                  textConfirm: '保存',
+                  textCancel: '取消',
+                  onConfirm: () {
+                    Get.snackbar('保存？', '确定保存配置吗？');
+                  },
+                  onCancel: () {
+                    Get.back();
+                  });
+            },
+            text: "配置信息",
+            shape: GFButtonShape.square,
+          ),
+          GFButton(
+            onPressed: () {
               SPUtil.remove("userinfo");
               SPUtil.remove("isLogin");
-              SPUtil.remove("userData");
               Navigator.popAndPushNamed(context, '/login');
             },
             text: "退出",
