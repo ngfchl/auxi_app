802fddca8c37e0db283ffdab21a21ca3a4a7de95
ngfchl <328176843@qq.com>
2023-11-22 14:21:11 +0800

    获取站点状态添加容错处理

--- lib/api/mysite.dart
+++ lib/api/mysite.dart
@@ -10,12 +10,18 @@ import 'api.dart';
 Future<CommonResponse> getSiteStatusList() async {
   final response = await DioClient().get(Api.MYSITE_STATUS_LIST);
   if (response.statusCode == 200) {
-    final siteStatusList = (response.data['data'] as List)
-        .map<SiteStatus>((item) => SiteStatus.fromJson(item))
-        .toList();
-    String msg = '共有${siteStatusList.length}个站点';
-    print(msg);
-    return CommonResponse(data: siteStatusList, code: 0, msg: msg);
+    try {
+      final siteStatusList = (response.data['data'] as List)
+          .map<SiteStatus>((item) => SiteStatus.fromJson(item))
+          .toList();
+      String msg = '共有${siteStatusList.length}个站点';
+      print(msg);
+      return CommonResponse(data: siteStatusList, code: 0, msg: msg);
+    } catch (err, trace) {
+      print(err);
+      Logger.instance.w(trace);
+      return CommonResponse(data: null, code: -1, msg: err.toString());
+    }
   } else {
     String msg = '获取主页状态失败: ${response.statusCode}';
     // GFToast.showToast(msg, context);
