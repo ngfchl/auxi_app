35a04af0a1b735afc203d14238243d0b623e01f0
ngfchl <328176843@qq.com>
2023-11-23 20:11:22 +0800

    取消动态背景

--- lib/common/glass_widget.dart
+++ lib/common/glass_widget.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
-import 'package:lottie/lottie.dart';
-
-import '../utils/string_utils.dart';
 
 class GlassWidget extends StatelessWidget {
   const GlassWidget({Key? key, required Widget this.child}) : super(key: key);
@@ -14,41 +11,12 @@ class GlassWidget extends StatelessWidget {
     return Stack(
       fit: StackFit.expand,
       children: [
-        Lottie.asset(
-          StringUtils.getLottieByName('rJoSLquA8J'),
-          repeat: true,
-          fit: BoxFit.fill,
-        ),
-        // GlassmorphicContainer(
-        //   width: double.infinity,
-        //   height: double.infinity,
-        //   borderRadius: 0,
-        //   linearGradient: LinearGradient(
-        //       colors: [
-        //         const Color(0xFFFFFFFF).withOpacity(0.1),
-        //         const Color(0xFFFFFFFF).withOpacity(0.05),
-        //       ],
-        //       begin: Alignment.topLeft,
-        //       end: Alignment.bottomRight,
-        //       stops: const [
-        //         0.01,
-        //         0.1,
-        //       ]),
-        //   border: 0,
-        //   blur: 20,
-        //   borderGradient: LinearGradient(
-        //       colors: [
-        //         const Color(0xFFFFFFFF).withOpacity(0.1),
-        //         const Color(0xFFFFFFFF).withOpacity(0.05),
-        //       ],
-        //       begin: Alignment.topLeft,
-        //       end: Alignment.bottomRight,
-        //       stops: const [
-        //         0.1,
-        //         1,
-        //       ]),
-        //   child: child!,
-        // )
+        // Lottie.asset(
+        //   StringUtils.getLottieByName('rJoSLquA8J'),
+        //   repeat: true,
+        //   fit: BoxFit.fill,
+        // ),
+
         GlassContainer(
           height: double.infinity,
           width: double.infinity,
@@ -58,10 +26,10 @@ class GlassWidget extends StatelessWidget {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
-              Colors.blueGrey.withOpacity(0.5),
+              Colors.green.withOpacity(0.5),
               Colors.blueGrey.withOpacity(0.6),
               Colors.grey.withOpacity(0.4),
-              Colors.black12.withOpacity(0.6),
+              Colors.blue.withOpacity(0.6),
             ],
           ),
           //--code to remove border
